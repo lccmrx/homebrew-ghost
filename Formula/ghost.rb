9e57c67960267196e6485d3319254b29bc8acdd1
Lucca Marques <contact@lccmrx.dev>
2025-09-30 02:17:17 -0300

    rel: release v0.0.5

--- Formula/ghost.rb
+++ Formula/ghost.rb
@@ -1,14 +1,15 @@
 class Ghost < Formula
   desc "Simple local DNS + proxy manager"
   homepage "https://github.com/lccmrx/ghost"
-  version "0.0.4"
-  url "https://github.com/lccmrx/ghost/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "7ce933815d8f7947906715c374f917b108e206d441b330205e77c1f728a9d5cc"
+  version "0.0.5"
+  url "https://github.com/lccmrx/ghost/archive/refs/tags/v0.0.5.tar.gz"
+  sha256 "f28e2ec6f7268b021a342bdb650de61b048e7ab24229e42ef541b0c5d285ef2b"
   license "Apache"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "ghost.go"
+    system "go", "build", "ghost.go"
+    bin.install "ghost"
   end
 end
